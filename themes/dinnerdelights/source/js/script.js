9b7c58a89a8e23565e8a6d2e88b5882df74a6163
Shane Church <shane@s-church.net>
2017-01-02 22:17:21 -0600

    Moving content into partials. Tweaking CSS colors

--- themes/dinnerdelights/source/js/script.js
+++ themes/dinnerdelights/source/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	var headerHeight = $('header').height();
 
 	//Smooth Scroll
 	$(function() {
@@ -8,7 +9,7 @@ $(document).ready(function() {
 				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 				if (target.length) {
 					$('html, body').animate({
-						scrollTop: target.offset().top
+					scrollTop: target.offset().top - headerHeight
 					}, 1000);
 					return false;
 				}
@@ -16,20 +17,11 @@ $(document).ready(function() {
 		});
 	});
 
-
 	// Main Menu
 	$('#main-nav').affix({
 		offset: {
 			top: $('header').height()
 		}
-	});
-
-
-	// Top Search
-	$("#ss").click(function(e) {
-		e.preventDefault();
-		$(this).toggleClass('current');
-		$(".search-form").toggleClass('visible');
 	});
 
 
@@ -47,43 +39,4 @@ $(document).ready(function() {
 		left: positionPad + 'px',
 		marginTop: '-' + $('.caption').height() / 2 + 'px'
 	});
-
-
-	// number effect
-	$('.about-bg-heading').one('inview', function(event, visible) {
-		if (visible == true) {
-			$('.count').each(function() {
-				$(this).prop('Counter', 0).animate({
-					Counter: $(this).text()
-				}, {
-					duration: 5000,
-					easing: 'swing',
-					step: function(now) {
-						$(this).text(Math.ceil(now));
-					}
-				});
-			});
-		}
-	});
-
-
-	//Google Map
-    var get_latitude = $('#google-map').data('latitude');
-    var get_longitude = $('#google-map').data('longitude');
-
-    function initialize_google_map() {
-        var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
-        var mapOptions = {
-            zoom: 14,
-            scrollwheel: false,
-            center: myLatlng
-        };
-        var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map
-        });
-    }
-    google.maps.event.addDomListener(window, 'load', initialize_google_map);
-
 });
