06bcfa516c77e4643265c3e243f190bfe80af6b4
Shane Church <shane@s-church.net>
2020-03-28 18:22:47 -0500

    Styled Availability

--- components/Availability.vue
+++ components/Availability.vue
@@ -1,27 +1,50 @@
 <template>
-  <div id="availability" class="availability" v-editable="blok">
-    <nuxt-link :to="{ path: '/', hash: '#contact' }" class="btn btn-primary"
-      >CONTACT ME TO SCHEDULE TODAY!</nuxt-link
-    >
-    <br />
-    <br />
-    <iframe
-      src="https://calendar.zoho.com/embed/08dc77ba1c0be80167b51905aa1f9e3881c8f120c1a7eebd5a3cdb8275278a42f179b5a452895f3c875a991ebddcddd4"
-      style="width:100%;height:500px"
-    ></iframe>
-    <br />
-    <br />
-    <nuxt-link :to="{ path: '/', hash: '#contact' }" class="btn btn-primary"
-      >CONTACT ME TO SCHEDULE TODAY!</nuxt-link
-    >
+  <div>
+    <Masthead></Masthead>
+    <div class="container">
+      <div id="availability" class="availability">
+        <nuxt-link :to="{ path: '/', hash: '#contact' }" class="btn btn-primary"
+          >CONTACT ME TO SCHEDULE TODAY!</nuxt-link
+        >
+        <iframe
+          src="https://calendar.zoho.com/embed/08dc77ba1c0be80167b51905aa1f9e3881c8f120c1a7eebd5a3cdb8275278a42f179b5a452895f3c875a991ebddcddd4"
+        ></iframe>
+        <nuxt-link :to="{ path: '/', hash: '#contact' }" class="btn btn-primary"
+          >CONTACT ME TO SCHEDULE TODAY!</nuxt-link
+        >
+      </div>
+    </div>
+    <PageFooter></PageFooter>
   </div>
 </template>
 
 <script>
+import Masthead from "./Masthead.vue";
+import PageFooter from "./PageFooter.vue";
+
 export default {
-  props: ["blok"]
+  props: ["blok"],
+  components: {
+    Masthead,
+    PageFooter
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+.availability {
+  display: flex;
+  flex-direction: column;
+
+  .btn {
+    align-self: center;
+    flex: 0 0 auto;
+    margin: 16px auto;
+  }
+
+  iframe {
+    flex: 1 1 auto;
+    min-height: 500px;
+  }
+}
 </style>
--- components/Contact.vue
+++ components/Contact.vue
@@ -41,6 +41,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import "~assets/styles/variables.scss";
+
 .contact {
   .icon {
     width: 26px;
@@ -48,6 +50,77 @@ export default {
     align-items: center;
     justify-content: center;
     margin-right: 12px;
+  }
+}
+
+.validation {
+  color: red;
+  display: none;
+  margin: 0 0 20px;
+  font-weight: 400;
+  font-size: 13px;
+}
+
+#sendmessage {
+  color: green;
+  border: 1px solid green;
+  display: none;
+  text-align: center;
+  padding: 15px;
+  font-weight: 600;
+  margin-bottom: 15px;
+}
+
+#errormessage {
+  color: red;
+  display: none;
+  border: 1px solid red;
+  text-align: center;
+  padding: 15px;
+  font-weight: 600;
+  margin-bottom: 15px;
+}
+
+#sendmessage.show,
+#errormessage.show,
+.show {
+  display: block;
+}
+
+/* Contact */
+
+.contact-container {
+  line-height: 2;
+}
+
+#mc-embedded-subscribe {
+  margin-top: 16px;
+}
+
+#mc_embed_signup {
+  margin-top: 50px;
+}
+
+input.mce_inline_error {
+  border: 2px solid $primary-pepper !important;
+}
+
+div.mce_inline_error {
+  color: $primary-pepper !important;
+  background: none !important;
+  font-size: 16px !important;
+  font-weight: normal !important;
+}
+
+@media (min-width: 768px) {
+  .contact-container {
+    font-size: 1.6em;
+  }
+}
+
+@media (min-width: 992px) {
+  #mc_embed_signup {
+    margin-top: 0;
   }
 }
 </style>
