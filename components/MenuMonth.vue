99e4793c08a9d21b9530c73e97dd77c3437a6803
Shane Church <shane@s-church.net>
2020-10-25 22:21:26 -0500

    Added header and footer options for Menu Month

--- components/MenuMonth.vue
+++ components/MenuMonth.vue
@@ -1,12 +1,13 @@
 <template>
   <div v-editable="blok">
     <h3 class="subheading">{{ blok.title }}</h3>
-
+    <rich-text v-if="blok.header && blok.header.content && blok.header.content.length > 0" :text="blok.header"></rich-text>
     <ul class="menu-list">
       <li v-for="(item, index) in blok.items" :key="index">
         <blok-menu-item :blok="item" :index="index"></blok-menu-item>
       </li>
     </ul>
+    <rich-text v-if="blok.footer && blok.footer.content && blok.footer.content.length > 0" :text="blok.footer"></rich-text>
   </div>
 </template>
 
